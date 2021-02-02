dab47f3c815cf42fd310917ee05b4f3992644b00
Mario <mario.guillermo950@gmail.com>
2021-02-02 16:41:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/registerform/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.registerform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.registerform.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private String getTrimmedText(EditText editText){
+         return editText.getText() == null ? "" : editText.getText().toString();
+    }
+}
